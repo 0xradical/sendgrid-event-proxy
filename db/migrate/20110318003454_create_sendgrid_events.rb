f82ba239ca46922b4aeb485fffeff61e1caca83f
thiagobrandam <thiago@thiagobrandam.local>
2011-03-17 22:01:26 -0300

    Rails db stuff

---
+++ db/migrate/20110318003454_create_sendgrid_events.rb
@@ -0,0 +1,19 @@
+class CreateSendgridEvents < ActiveRecord::Migration
+  def self.up
+    create_table :sendgrid_events do |table|
+      table.string :event    
+      table.string :email    
+      table.string :category 
+      table.string :reason  
+      table.string :response
+      table.string :attempt  
+      table.string :type   
+      table.string :status
+      table.string :url      
+    end
+  end
+
+  def self.down
+    drop_table :sendgrid_events
+  end
+end
