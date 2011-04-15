55485398d705acb0367994c3417fadfd58f4c18c
thiagobrandam <thiago@thiagobrandam.local>
2011-04-15 03:23:09 -0300

    removed unnecessary files

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-SendgridEventProxy::Application.config.secret_token = '07c0217da95214b1ea864b7369c3398e646fd3c45db5d09b194eabe33cf33616fe5b9a8b88d43fafef7f2a33ea6bacc897aa8a5f8954762ad0ae75d8b18f9224'
