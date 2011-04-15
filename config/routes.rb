61a979b69330b2f5457acb6d39935d18b38e777d
thiagobrandam <thiago@thiago.redealumni>
2011-04-15 17:15:53 -0300

    don't show anything through any GET requests

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ SendgridEventProxy::Application.routes.draw do
   
   match 'sendgrid_event' => 'sendgrid#event', :via => :post
 
+  match '/' => proc { |env| [200, {}, ""] }
+    
+  match ':controller(/:action(/:id))' => proc { |env| [200, {}, ""] }, :constraints => {:id => /.*/}  
+
 end
