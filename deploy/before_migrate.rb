632c5beee4120f4f3f84799ede7f66989db12797
thiagobrandam <thiago@thiago.redealumni>
2011-04-15 15:08:11 -0300

    test 5

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,8 +1,3 @@
 run "cd #{release_path} && bundle install"
 
-run "cd #{release_path} && CAPTA_GEMPATH=`bundle show curb`"
-
-capta_gempath_warning = "Deployment aborted: the gem capta is probably pointing to the vendor application folder " +
-                        "when it should be pointing to the git repository. Check your Gemfile and retry."
-
-run "/usr/bin/env ruby -e \"capta_gempath = '`echo $CAPTA_GEMPATH`' ; raise '#{capta_gempath_warning}'\""
+run "cd #{release_path} && bundle exec test_control_file.rb"
